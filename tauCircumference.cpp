ce37abee0a54a4d90bc7629e1387ae4c05e9ae49
Samuel-Webster-178 <samuel.webster@mths.ca>
2022-03-07 19:18:42 +0000

    init

--- tauCircumference.cpp
+++ tauCircumference.cpp
@@ -12,12 +12,16 @@ int main() {
     const double TAU = 6.28;
     int radius;
     double circumference;
+
     // input
     std::cout << "Enter radius of the circle in mm: ";
     std::cin >> radius;
+
     // process
     circumference = TAU*radius;
+
     // output
     std::cout << "" << std::endl;
     std::cout << "Circumference is " << circumference << " mm" << std::endl;
+    std::cout << "\nDone." << std::endl;
 }
